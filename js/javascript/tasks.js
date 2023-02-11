48e529ca4762fec6874eb5f85ad9e4fd2a4ddc1c
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-02-11 23:00:26 +0200

    28 tasks

---
+++ js/javascript/tasks.js
@@ -0,0 +1,348 @@
+// автоперевірка 7
+// function sayHi() {
+//   console.log('Hello, this is my first function!');
+// }
+// sayHi();
+
+// автоперевірка 8
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// автоперевірка 9
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+
+// add(2, 5, 8);
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// автоперевірка 10
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   console.log(message);
+//   return message;
+// }
+// makeMessage('Radar', 6150);
+// makeMessage('Scanner', 3500);
+// makeMessage('Reactor', 8000);
+// makeMessage('Engine', 4070);
+
+// автоперевірка 11
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   const totalPrice = orderedQuantity * pricePerItem;
+//   console.log(totalPrice);
+//   return totalPrice;
+// }
+
+// calculateTotalPrice(5, 100);
+// calculateTotalPrice(8, 60);
+// calculateTotalPrice(3, 400);
+// calculateTotalPrice(1, 3500);
+// calculateTotalPrice(12, 70);
+
+// автоперевірка 12
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const massage = `You ordered droids worth ${
+//     pricePerDroid * orderedQuantity + deliveryFee
+//   } credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   console.log(massage);
+//   return massage;
+// }
+// makeOrderMessage(2, 100, 50);
+// makeOrderMessage(4, 300, 100);
+// makeOrderMessage(10, 70, 200);
+
+// автоперевірка 13
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
+//   console.log(passed);
+//   // Change code above this line
+//   return passed;
+// }
+// isAdult(20);
+// isAdult(14);
+// isAdult(8);
+// isAdult(37);
+
+// автоперевірка 14
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+
+//   const isMatch = password === 'jqueryismyjam';
+//   console.log(isMatch);
+
+//   return isMatch;
+// }
+// isValidPassword('mangodab3st');
+// isValidPassword('kiwirul3z');
+// isValidPassword('jqueryismyjam');
+
+// автоперевірка 15
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   console.log(message);
+//   return message;
+// }
+
+// checkAge(20);
+// checkAge(8);
+// checkAge(14);
+// checkAge(38);
+
+// автоперевірка 16
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (available < ordered) {
+//     message = 'Not enough goods in stock!';
+//   }
+//   // Change code above this line
+//   else {
+//     message = 'Order is processed, our manager will contact you.';
+//   }
+//   console.log(message);
+//   return message;
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(200, 20);
+// checkStorage(200, 150);
+// checkStorage(150, 180);
+
+// автоперевірка 17
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// автоперевірка 18
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   const totalPrice = pricePerDroid * orderedQuantity;
+//   if (totalPrice > customerCredits) {
+//     message = `Insufficient funds!`;
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   }
+//   console.log(message);
+//   return message;
+// }
+
+// makeTransaction(3000, 5, 23000);
+// makeTransaction(1000, 3, 15000);
+// makeTransaction(5000, 10, 8000);
+// makeTransaction(2000, 8, 10000);
+// makeTransaction(500, 10, 5000);
+
+// автоперевірка 19
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   if (password === null) {
+//     message = 'Canceled by user!';
+//   } else if (password === 'jqueryismyjam') {
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+//   console.log(message);
+//   return message;
+// }
+
+// checkPassword('mangohackzor');
+// checkPassword(null);
+// checkPassword('polyhax');
+// checkPassword('jqueryismyjam');
+
+// автоперевірка 20
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered === 0) {
+//     message = 'There are no products in the order!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+//   console.log(message);
+//   return message;
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(70, 0);
+// checkStorage(200, 20);
+// checkStorage(200, 250);
+// checkStorage(150, 0);
+
+// автоперевірка 21
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   console.log(isInRange);
+//   return isInRange;
+// }
+// isNumberInRange(10, 30, 17);
+// isNumberInRange(10, 30, 5);
+// isNumberInRange(20, 50, 24);
+// isNumberInRange(20, 50, 76);
+
+// автоперевірка 21
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === 'pro' || subType === 'vip'; // Change this line
+//   console.log(canAccessContent);
+//   return canAccessContent;
+// }
+// checkIfCanAccessContent('pro');
+// checkIfCanAccessContent('starter');
+// checkIfCanAccessContent('free');
+
+// автоперевірка 23
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange; // Change this line
+
+//   return isNotInRange;
+// }
+// isNumberNotInRange(10, 30, 17);
+// isNumberNotInRange(10, 30, 5);
+// isNumberNotInRange(20, 50, 24);
+// isNumberNotInRange(20, 50, 76);
+
+// автоперевірка 24
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+
+//   if (totalSpent < 5000) {
+//     discount = BASE_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent < 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent < 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else {
+//     discount = GOLD_DISCOUNT;
+//   }
+
+//   console.log(discount);
+//   return discount;
+// }
+// getDiscount(137000);
+// getDiscount(46900);
+// getDiscount(8250);
+// getDiscount(1300);
+// getDiscount(5000);
+// getDiscount(20000);
+// getDiscount(50000);
+
+// автоперевірка 25
+// function checkStorage(available, ordered) {
+//   let message;
+
+//   message =
+//     ordered > available
+//       ? 'Not enough goods in stock!'
+//       : 'The order is accepted, our manager will contact you';
+
+//   console.log(message);
+//   return message;
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(200, 20);
+// checkStorage(200, 150);
+// checkStorage(150, 180);
+
+// автоперевірка 26
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   // Change code below this line
+//   message = ADMIN_PASSWORD === password ? 'Access is allowed' : 'Access denied, wrong password!';
+//   // Change code above this line
+//   console.log(message);
+//   return message;
+// }
+// checkPassword('jqueryismyjam');
+// checkPassword('angul4r1sl1f3');
+// checkPassword('r3actsux');
+
+// автоперевірка 27
+// function getSubscriptionPrice(type) {
+//   let price;
+
+//   switch (type) {
+//     case 'starter':
+//       price = 0;
+//       break;
+
+//     case 'professional':
+//       price = 20;
+//       break;
+
+//     case 'organization':
+//       price = 50;
+//       break;
+//   }
+//   console.log(price);
+//   return price;
+// }
+
+// getSubscriptionPrice('professional');
+// getSubscriptionPrice('organization');
+// getSubscriptionPrice('starter');
+
+// автоперевірка 28
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  switch (password) {
+    case null:
+      message = 'Canceled by user!';
+      break;
+    case ADMIN_PASSWORD:
+      message = 'Welcome!';
+      break;
+    default:
+      message = 'Access denied, wrong password!';
+  }
+
+  console.log(message);
+  return message;
+}
+checkPassword('mangohackzor');
+checkPassword(null);
+checkPassword('polyhax');
+checkPassword('jqueryismyjam');
