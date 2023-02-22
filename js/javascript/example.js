96801e4891197542a52c5ce1a2ab091df21efb59
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-02-23 00:30:02 +0200

    module 3

--- js/javascript/example.js
+++ js/javascript/example.js
@@ -193,23 +193,239 @@ const friends = [
 // };
 // console.log(getAllNames(friends));
 
-const friendsOnline = function (allFriends) {
-  const onlineFriend = [];
+// const friendsOnline = function (allFriends) {
+//   const onlineFriend = [];
 
-  //   for (const friend of allFriends) {
-  //     console.log(friend.online);
-  //     if (friend.online === true) {
-  //       console.log('иди спать!');
-  //     }
-  //   }
+//   //   for (const friend of allFriends) {
+//   //     console.log(friend.online);
+//   //     if (friend.online === true) {
+//   //       console.log('иди спать!');
+//   //     }
+//   //   }
 
-  for (const friend of allFriends) {
-    if (friend.online) {
-      //if (!friend.online)// не онлайн, проверка на false
-      onlineFriend.push(friend.name); //Не надо проверять на тру
-      console.log(onlineFriend); //Kiwi
-    }
-  }
-};
+//   for (const friend of allFriends) {
+//     if (friend.online) {
+//       //if (!friend.online)// не онлайн, проверка на false
+//       onlineFriend.push(friend.name); //Не надо проверять на тру
+//       console.log(onlineFriend); //Kiwi
+//     }
+//   }
+// };
 
-console.log(friendsOnline(friends));
+// console.log(friendsOnline(friends));
+
+// ******************
+//SPREAD//
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консолі буде масив
+// // console.log(temps);
+// // ❌ Так не спрацює, тому що передаємо цілий масив
+// // console.log(Math.max(temps)); // NaN
+
+// // В консолі буде набір окремих чисел
+// console.log(...temps);
+// // ✅ Розподілимо колекцію елементів у якості окремих аргументів
+// console.log(Math.max(...temps)); // 25
+// console.log(Math.max(...[14, -4, 25, 8, 11])); //25
+
+// const temps = [14, -4, 25, 8, 11];
+
+// // Це точна, але незалежна копія масиву temps
+// const copyOfTemps = [...temps];
+// console.log(copyOfTemps); // [14, -4, 25, 8, 11]
+
+// const lastWeekTemps = [14, 25, 11];
+// const currentWeekTemps = [23, 17, 18];
+// const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+// console.log(allTemps); // [14, 25, 11, 23, 17, 18]
+// Операція ... (spread) дозволяє розподілити властивості довільної кількості об'єктів в один новий.
+// const first = { propA: 5, propB: 10 };
+// const second = { propC: 15 };
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
+
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15, propD: 20 }
+
+// const fourth = { ...second, ...first };
+// console.log(fourth); // { propA: 5, propB: 10, propC: 50, propD: 20 }
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15 };
+
+// const third = { propB: 20, ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+// const fourth = { ...first, ...second, propB: 20 };
+// console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+// const fifth = { ...first, propB: 20, ...second };
+// console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
+
+// ********REST*******
+
+// function multiply(...args) {
+//   console.log(args); // масив усіх аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+// console.log(firstNumber); // Значення першого аргументу
+// console.log(secondNumber); // Значення другого аргументу
+// console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+// ************************
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// const accessType = book.isPublic ? 'публічному' : 'закритому';
+// console.log(`Книга ${book.title} автора ${book.author} з рейтингом ${book.rating}
+// знаходиться в ${accessType} доступі.`);
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// Деструктуризуємо
+// const { title, author, isPublic, rating, coverImage } = book;
+// console.log(coverImage); // undefined
+
+// const accessType = isPublic ? 'публічному' : 'закритому';
+// const message = `Книга ${title} автора ${author} з рейтингом ${rating} знаходиться в ${accessType} доступі.`;
+// console.log(message);
+
+// const firstBook = {
+//   title: 'The Last Kingdom',
+//   coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg',
+// };
+
+// const { title: firstTitle, coverImage: firstCoverImage = 'https://via.placeholder.com/640/480' } =
+//   firstBook;
+
+// console.log(firstBook);
+// // console.log(firstTitle); // The Last Kingdom
+// // console.log(firstCoverImage); // https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
+
+// const secondBook = {
+//   title: 'Сон смішної людини',
+// };
+
+// const { title: secondTitle, coverImage: secondCoverImage = 'https://via.placeholder.com/640/480' } =
+//   secondBook;
+
+// console.log(secondTitle); // Сон смішної людини
+// console.log(secondCoverImage); // https://via.placeholder.com/640/480
+// console.log(secondBook);
+// *****************************
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На березі спокійних вод',
+//     author: 'Роберт Шеклі',
+//     rating: 8.51,
+//   },
+// ];
+
+// // for (const book of books) {
+// //   console.log(book.title);
+// //   // console.log(book.author);
+// //   // console.log(book.rating);
+// // }
+
+// for (const book of books) {
+//   const { title, author, rating } = book;
+
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+// ********тоже самое
+// for (const { title, author, rating } of books) {
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+
+// Глибока деструктуризація
+// Для деструктуризації властивостей вкладених об'єктів використовуються
+// ті самі принципи, що й в трьох попередніх вправах.
+
+// const user = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views, likes },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(views); // 4827
+// console.log(likes); // 1308
+
+// const {
+//   name,
+//   tag,
+//   stats: { followers, views: userViews, likes: userLikes = 0 },
+// } = user;
+
+// console.log(name); // Jacques Gluke
+// console.log(tag); // jgluke
+// console.log(followers); // 5603
+// console.log(userViews); // 4827
+// console.log(userLikes); // 1308
+
+// ******************
+// const rgb = [200, 255, 100];
+// const [red, green, blue] = rgb;
+// console.log(`R:${red},G:${green},B:${blue}`); // "R:200,G:255,B:100"
+
+// const rgb = [200, 100, 255];
+// const [red, green, blue, alfa = 0.3] = rgb;
+// console.log(`R:${red},G:${green},B:${blue},Alfa:${alfa}`); // "R:200,G:100,B:255,Alfa:0.3"
+
+// const rgb = [200, 255, 100];
+// const [red, ...colors] = rgb;
+// console.log(red); // "200"
+// console.log(colors); // [255, 100]
+
+// const rgb = [200, 100, 255];
+// const [, , blue] = rgb;
+// console.log(`Blue: ${blue}`); // "Blue: 255"
+
+// **********************
--- js/javascript/tasks.js
+++ js/javascript/tasks.js
@@ -912,3 +912,36 @@
 //   console.log(keys);
 //   console.log(values);
 // }
+
+// ***********
+// автоперевірка 16
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   console.log(values);
+//   for (const value of values) totalSalary += value;
+//   console.log(totalSalary);
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 });
+
+// ***********
+// автоперевірка 17
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
