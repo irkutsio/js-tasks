23648d768d9c0bafe8bfe252dcd271d0e6ed85f5
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-03-24 00:24:40 +0200

    input

--- js/javascript/example.js
+++ js/javascript/example.js
@@ -1324,22 +1324,37 @@ const arr = [
 // console.log(myArray);
 
 // DOM***********************************************************************************************************************
-пример с кликами на кнопку!!!
-const decBtn = document.querySelector('[data-action="decrement"]');
-const incBtn = document.querySelector('[data-action="increment"]');
-const newBtn = document.querySelector('[data-action="crement"]');
+// пример с кликами на кнопку!!!
+// const decBtn = document.querySelector('[data-action="decrement"]');
+// const incBtn = document.querySelector('[data-action="increment"]');
+// const newBtn = document.querySelector('[data-action="crement"]');
 
-incBtn.addEventListener('click', event => {
-  console.log(event);
-  console.log('клик на плюс');
-  decBtn.addEventListener('click', onDecrementClick);
-});
+// incBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('клик на плюс');
+//   decBtn.addEventListener('click', onDecrementClick);
+// });
 
-newBtn.addEventListener('click', event => {
-  console.log('убираем клик на минус');
-  decBtn.removeEventListener('click', onDecrementClick);
-});
+// newBtn.addEventListener('click', event => {
+//   console.log('убираем клик на минус');
+//   decBtn.removeEventListener('click', onDecrementClick);
+// });
 
-function onDecrementClick(event) {
-  console.log('клик на минус');
-}
+// function onDecrementClick(event) {
+//   console.log('клик на минус');
+// }
+
+// с инпутом
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+// inputEl.addEventListener("focus", onInputFocus);
+// inputEl.addEventListener("blur", onInputBlur);
+
+// function onInputFocus() {
+//   inputEl.style.borderColor = "violet";
+//   console.log("сделали фокус");
+// }
+// function onInputBlur() {
+//   console.log("сделали блюр - убрали фокус");
+// }
