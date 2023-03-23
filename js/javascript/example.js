332618d22e7f17210cb7279620912ec01563a1c5
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-03-23 23:20:10 +0200

    dom

--- js/javascript/example.js
+++ js/javascript/example.js
@@ -1324,21 +1324,22 @@ const arr = [
 // console.log(myArray);
 
 // DOM***********************************************************************************************************************
-// пример с кликами на кнопку!!!
-// const decBtn = document.querySelector('[data-action="decrement"]');
-// const incBtn = document.querySelector('[data-action="increment"]');
-// const newBtn = document.querySelector('[data-action="crement"]');
+пример с кликами на кнопку!!!
+const decBtn = document.querySelector('[data-action="decrement"]');
+const incBtn = document.querySelector('[data-action="increment"]');
+const newBtn = document.querySelector('[data-action="crement"]');
 
-// incBtn.addEventListener('click', () => {
-//   console.log('клик на плюс');
-//   decBtn.addEventListener('click', onDecrementClick);
-// });
+incBtn.addEventListener('click', event => {
+  console.log(event);
+  console.log('клик на плюс');
+  decBtn.addEventListener('click', onDecrementClick);
+});
 
-// newBtn.addEventListener('click', () => {
-//   console.log('убираем клик на минус');
-//   decBtn.removeEventListener('click', onDecrementClick);
-// });
+newBtn.addEventListener('click', event => {
+  console.log('убираем клик на минус');
+  decBtn.removeEventListener('click', onDecrementClick);
+});
 
-// function onDecrementClick() {
-//   console.log('клик на минус');
-// }
+function onDecrementClick(event) {
+  console.log('клик на минус');
+}
