c3d93407582a1d0d39bc14c10343d1c11e8ebf92
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-02 23:59:19 +0300

    commit

--- js/javascript/example.js
+++ js/javascript/example.js
@@ -8,7 +8,7 @@
 //   } else {
 //     return (add = sum(arr, n - 1) + arr[n - 1]);
 //   }
-//   // Змініть код лише над цим рядком
+//       // Змініть код лише над цим рядком
 // }
 // sum([2, 3, 4], 1);
 // sum([2, 3, 4, 5], 3);
@@ -1058,14 +1058,14 @@
 const arr = [
   { name: 'Albert', surname: 'Einstein', born: 1879, dead: 1955, id: 1 },
   { name: 'Isaac', surname: 'Newton', born: 1643, dead: 1727, id: 2 },
-  { name: 'Galileo', surname: 'Galilei', born: 1564, dead: 1642, id: 3 },
+  { name: 'Galileo', surname:   'Galilei', born: 1564, dead: 1642, id: 3 },
   { name: 'Marie', surname: 'Curie', born: 1867, dead: 1934, id: 4 },
   { name: 'Pierre', surname: 'Curie', born: 1859, dead: 1906, id: 5 },
   { name: 'Johannes', surname: 'Kepler', born: 1571, dead: 1630, id: 6 },
   { name: 'Nicolaus', surname: 'Copernicus', born: 1473, dead: 1543, id: 7 },
   { name: 'Max', surname: 'Planck', born: 1858, dead: 1947, id: 8 },
   { name: 'Katherine', surname: 'Blodgett', born: 1898, dead: 1979, id: 9 },
-  { name: 'Ada', surname: 'Lovelace', born: 1815, dead: 1852, id: 10 },
+  { name: 'Ada', surname: 'Lovelace' , born: 1815, dead: 1852, id: 10 },
   { name: 'Sarah E.', surname: 'Goode', born: 1855, dead: 1905, id: 11 },
   { name: 'Lise', surname: 'Meitner', born: 1878, dead: 1968, id: 12 },
   { name: 'Hanna', surname: 'Hammarström', born: 1829, dead: 1909, id: 13 },
